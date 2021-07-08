ddc1e673206a17cb4e6939e696a3a923fa9c07ba
Akash Patel <akash.patel@vortilla.com>
2021-07-08 14:46:29 +0530

    Ad the text

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,7 +4,9 @@ function aboutpage() {
 function homepage() {
   window.location.href ="index.html" ;
 }
-
+function contactpage() {
+  window.location.href ="contact.html" ;
+}
 // By:
 // Manuel Pinto
 // https://manuelpinto.in
